41c0aa5e65c1e27102f95eaead315a7eb74e7cf6
Carl <carl@carlossus.com>
2016-09-03 23:49:10 +0800

    Update go script

--- go.sh
+++ go.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Fetch Go 1.7
-sudo curl -s https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz >> /home/vagrant/go.tar.gz
+sudo curl -s https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz >> $HOME/go.tar.gz
 
 # Unpack the archive
-(cd /home/vagrant && sudo tar -xvf go.tar.gz)
-
-# Create a symlink so 'go' works from the command line
-sudo ln -s /home/vagrant/go/bin/go /usr/bin/go
+(cd $HOME && sudo tar -xvf go.tar.gz)
 
 # Create $GOPATH
-touch /home/vagrant/.bashrc
-echo "export GOPATH=/home/vagrant/gotools" >> /home/vagrant/.bashrc
-echo "export GOROOT=/home/vagrant/go" >> /home/vagrant/.bashrc
-source /home/vagrant/.bashrc
+touch $HOME/.bashrc
+echo "export PATH=\"$HOME/go/bin:\$PATH\"" >> $HOME/.bashrc
+echo "export GOPATH=\"$HOME/projects/go\"" >> $HOME/.bashrc
+echo "export GOROOT=\"$HOME/go\"" >> $HOME/.bashrc
+source $HOME/.bashrc
