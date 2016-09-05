4e6c2ff5fc0be2274c6622d5ed384c440840afb2
Carl <carl@carlossus.com>
2016-09-05 22:16:16 +0800

    Remove main testing code

--- main.go
+++ main.go
@@ -1,33 +1,7 @@
-package docktor
+package main
 
-import (
-	"fmt"
-
-	dockerClient "github.com/fsouza/go-dockerclient"
-)
-
-const (
-	dockerEndpoint = "unix:///var/run/docker.sock"
-)
+import web "github.com/seikho/docktor/web"
 
 func main() {
-	connect(Docker{path: dockerEndpoint})
-}
-
-func connect(options Docker) {
-	client, err := dockerClient.NewClient(options.path)
-	if err != nil {
-		panic("Unable to connect to Docker")
-	}
-	images, _ := client.ListImages(dockerClient.ListImagesOptions{})
-
-	for _, image := range images {
-		fmt.Printf(image.ID + "\n")
-	}
-}
-
-// Docker options for connecting to
-type Docker struct {
-	path string // unix socket path. defaults to unix:///var/run/docker.sock
-
+	web.Start()
 }
